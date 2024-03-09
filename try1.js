14ac0d83e28993d7862034fbed76de27732bc607
Long <longpangorn@gmail.com>
2024-03-09 15:46:06 +0700

    try commit

---
+++ try1.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-bar')[0]
+const navbarLists = document.getElementsByClassName('navbar-lists')[0]
+
+toggleButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    navbarLists.classList.toggle('active')
+})
