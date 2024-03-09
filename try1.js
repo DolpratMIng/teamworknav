21e29aeeba550f3579e323b4ffdfc94a49f2c123
long104 <longpantorn@gmail.com>
2024-03-09 16:23:10 +0700

    javachanage

--- try1.js
+++ try1.js
@@ -1,7 +1,9 @@
-const toggleButton = document.getElementsByClassName('toggle-bar')[0]
-const navbarLists = document.getElementsByClassName('navbar-lists')[0]
+const toggleButton = document.getElementsByClassName("toggle-bar")[0];
+const navbarLists = document.getElementsByClassName("navbar-lists")[0];
 
-toggleButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    navbarLists.classList.toggle('active')
-})
+toggleButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	navbarLists.classList.toggle("active");
+});
+
+console.log("hi");
